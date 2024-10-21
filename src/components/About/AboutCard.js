5ed22aa36170e6a641f9687e128b25b21a680954
Sabareshwaran <91449408+sabaresh-23@users.noreply.github.com>
2024-10-22 01:07:03 +0530

    Add files via upload

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Sabareshwaran L </span>
+            from <span className="purple">Madurai,Tamilnadu, India.</span>
+            <br />
+            I am currently seeking opportunities as a software developer.
+            <br />
+            I am pursuing B.E Computer Science and Engineering
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Sabareshwaran</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
